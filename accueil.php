33f8c4162df4377bea04778f71018e277d8cb5b2
Hugo Clavinas <hugo.clavinas@bewan.be>
2021-12-13 15:25:38 +0100

    debut du projet

---
+++ accueil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Accueil </title>
+</head>
+<body>
+    <div class="page">
+
+        <header>
+            <div class="presentation_profil">
+                
+            </div>
+        </header>
+
+        <div class="container"></div>
+
+        <footer></footer>
+
+    </div>
+</body>
+</html>
